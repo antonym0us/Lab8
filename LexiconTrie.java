d120d573dd895eb136a35d53a5ba10a7afefcdec
Shivam Patel <shivam0198@users.noreply.github.com>
2017-04-22 12:34:44 -0400

    Implemented addWord(), containsWord(), and containsPrefix()

--- LexiconTrie.java
+++ LexiconTrie.java
@@ -2,13 +2,72 @@ import structure5.*;
 import java.util.Iterator;
 
 public class LexiconTrie implements Lexicon {
-    public boolean addWord(String word) {return true;}
-    public int addWordsFromFile(String filename) {return 0;}
-    public boolean removeWord(String word) {return true;}
-    public int numWords() {return 0;}
-    public boolean containsWord(String word){return true;}
-    public boolean containsPrefix(String prefix){return true;}
-    public Iterator<String> iterator() {return null;}
-    public Set<String> suggestCorrections(String target, int maxDistance) {return null;}
-    public Set<String> matchRegex(String pattern){return null;}
+
+    protected int numWords = 0;
+
+    protected LexiconNode root;
+
+    /* The constructor */
+    public LexiconTrie() {
+        root = new LexiconNode(null, true);
+    }
+
+    public boolean addWord(String word) {
+        return addWordHelper(word, root);
+    }
+
+    private boolean addWordHelper (String word, LexiconNode parent) {
+        if (word.length() == 0) {
+            ++numWords;
+            return true;
+        } else if (parent.getChild(word.charAt(0)) == null) {
+            parent.addChild(word.charAt(0));
+            return addWordHelper(word.substring(1), parent.getChild(word.charAt(0)));
+        } else {
+            return false;
+        }
+    }
+    
+    public int addWordsFromFile(String filename) {
+        return 0;
+    }
+    
+    public boolean removeWord(String word) {
+        --numWords;
+        return true;
+    }
+    
+    public int numWords() {
+        return numWords;
+    }
+    
+    public boolean containsWord(String word) {
+        return containsWordHelper(word, root);
+    }
+
+    private boolean containsHelper(String word, LexiconNode parent) {
+        if (word.length() == 0) {
+            return true;
+        } else if (parent.getChild(word.charAt(0) != null)) {
+            return containsWordHelper(word.substring(1), parent.getChild(word.charAt(0)));
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean containsPrefix(String prefix) {
+        return containsHelper(prefix, root);
+    }    
+    
+    public Iterator<String> iterator() {
+        return null;
+    }
+    
+    public Set<String> suggestCorrections(String target, int maxDistance) {
+        return null;
+    }
+    
+    public Set<String> matchRegex(String pattern) {
+        return null;
+    }
 }
