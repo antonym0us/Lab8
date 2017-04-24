0572c6f11e84f46698f49b898cb923aca3096f14
Shivam Patel <shivam0198@users.noreply.github.com>
2017-04-24 11:01:21 -0400

    Add files via upload

--- LexiconNode.java
+++ LexiconNode.java
@@ -9,7 +9,7 @@ import java.util.Iterator;
  * Defines the behavior for a LexiconNode, the element that
  * composes the LexiconTrie.
  *
- */  
+ */
 class LexiconNode implements Comparable<LexiconNode> {
     Character letter;
     boolean isWord;
@@ -17,10 +17,14 @@ class LexiconNode implements Comparable<LexiconNode> {
     
     /* Constructor */
     LexiconNode(Character letter, boolean isWord) {
-        this.letter = Character.toLowerCase(letter);
-        this.isWord = isWord;
+	this.letter = Character.toLowerCase(letter);
+	this.isWord = isWord;
 
-        children = new OrderedVector<LexiconNode>();
+	children = new OrderedVector<LexiconNode>();
+    }
+
+    LexiconNode(){
+	LexiconNode(null, false);
     }
 
     /*
@@ -28,55 +32,64 @@ class LexiconNode implements Comparable<LexiconNode> {
      * POST: Returns numerical comparison of the LexiconNodes' chars
      */
     public int compareTo(LexiconNode that) {
-        if (that != null) {
-            return this.letter.compareTo(that.letter());
-        } else {
-            System.out.println("Error: Invalid 'compareTo' call");
-            return 999;
-        }
-    } 
+	if (that != null) {
+	    return this.letter.compareTo(that.letter());
+	} else {
+	    System.out.println("Error: Invalid 'compareTo' call");
+	    return Integer.MAX_VALUE;
+	}
+    }
 
     /* Return letter stored by this LexiconNode */
     public char letter() {
 	return this.letter;
     }
 
+    /* Return boolean determining whether the LexiconNode(combined w/ nodes before it) is a word or not  */
+    public boolean isWord() {
+	return isWord;
+    }
+
+    public void setIsWord(boolean newIsWord) {
+	this.isWord = newIsWord;
+    }
+    
     /*
      * PRE:  'ln' is not null
      * POST: Adds LexiconNode child to correct position in child data structure
      */
     public void addChild(LexiconNode ln) {
-        if (ln != null) {
-            children.add(ln);
-        } else {
-            System.out.println("Error: Invalid 'addChild' call");
-        }
+	if (ln != null) {
+	    children.add(ln);
+	} else {
+	    System.out.println("Error: Invalid 'addChild' call");
+	}
     }
 
     /* Get LexiconNode child for 'ch' out of child data structure */
     public LexiconNode getChild(char ch) {
-        Iterator it = children.iterator();
-        while (it.hasNext()) {
-            LexiconNode next = (LexiconNode)it.next();
-            if (next.letter() == ch) {
-                return next;
-            }
-        }
-        System.out.println("Error: 'getChild' returned null!");
-        return null;
+	Iterator it = children.iterator();
+	while (it.hasNext()) {
+	    LexiconNode next = (LexiconNode)it.next();
+	    if (next.letter() == ch) {
+		return next;
+	    }
+	}
+	System.out.println("Error: 'getChild' returned null!");
+	return null;
     }
 
     /* Remove LexiconNode child for 'ch' from child data structure */
     public void removeChild(char ch) {
-        Iterator it = children.iterator();
-        while (it.hasNext()) {
-            LexiconNode next = (LexiconNode)it.next();
-            if (next.letter() == ch) {
-                children.remove(next);
-                return;
-            }
-        }
-        System.out.println("Error: 'removeChild' cannot find param!");
+	Iterator it = children.iterator();
+	while (it.hasNext()) {
+	    LexiconNode next = (LexiconNode)it.next();
+	    if (next.letter() == ch) {
+		children.remove(next);
+		return;
+	    }
+	}
+	System.out.println("Error: 'removeChild' cannot find param!");
     }
 
     /* Iterate over children */
@@ -86,18 +99,19 @@ class LexiconNode implements Comparable<LexiconNode> {
 
     /*DEBUG TOOL*/
     public static void main(String args[]) {
-        LexiconNode node = new LexiconNode('a', true);
+	LexiconNode node = new LexiconNode('a', true);
 
-        // Test addChild [Should return error]
-        node.addChild(new LexiconNode('s', true));
-        node.addChild(new LexiconNode('m', true));
-        node.addChild(null);
+	// Test addChild [Should return error]
+	node.addChild(new LexiconNode('s', true));
+	node.addChild(new LexiconNode('m', true));
+	node.addChild(null);
 
-        // Test getChild and compareTo [Should return 0]
-        System.out.println(node.getChild('s').compareTo(new LexiconNode('s', true)));
-
-        //Test removeChild [Should return error]
-        node.removeChild('m');
-        node.getChild('m');
+	// Test getChild and compareTo [Should return 0]
+	System.out.println(node.getChild('s').compareTo(new LexiconNode('s', true)));
+	
+	//Test removeChild [Should return error]
+	node.removeChild('m');
+	node.getChild('m');
     }
 }
+
--- LexiconTrie.java
+++ LexiconTrie.java
@@ -2,72 +2,94 @@ import structure5.*;
 import java.util.Iterator;
 
 public class LexiconTrie implements Lexicon {
-
     protected int numWords = 0;
-
     protected LexiconNode root;
 
-    /* The constructor */
-    public LexiconTrie() {
-        root = new LexiconNode(null, true);
+    public LexiconTrie(){
+	root = new LexiconNode();
     }
 
     public boolean addWord(String word) {
-        return addWordHelper(word, root);
+	return addWordHelper(word, root);
     }
-
-    private boolean addWordHelper (String word, LexiconNode parent) {
-        if (word.length() == 0) {
-            ++numWords;
-            return true;
-        } else if (parent.getChild(word.charAt(0)) == null) {
-            parent.addChild(word.charAt(0));
-            return addWordHelper(word.substring(1), parent.getChild(word.charAt(0)));
-        } else {
-            return false;
-        }
+    
+    protected boolean addWordHelper(String word, LexiconNode parent) {
+	if (word.length() == 0) {	    
+	    // set parent isWord to true
+	    parent.setIsWord(true);
+	    ++numWords;
+	    return true;
+	} else if (parent.getChild(word.charAt(0)) == null) {
+	    parent.addChild(new LexiconNode(word.charAt(0), false));
+	    return addWordHelper (word.substring(1), parent.getChild(word.charAt(0)));
+	} else {
+	    return false;
+	}
     }
     
     public int addWordsFromFile(String filename) {
-        return 0;
+	// Ay bruv check out the filestream doc
+	int preNumWords = numWords;
+
+	FileStream stream = new FileStream(filename);
+	Scanner in = new Scanner(stream);
+	while (in.hasNext()) {
+	    addWord(in.next());
+	}
+	return numWords - preNumWords;
     }
     
     public boolean removeWord(String word) {
-        --numWords;
-        return true;
+	if (word.length() == 0 && parent.isWord()) {
+	    parent.setIsWord(false);
+	    --numWords;
+	    return true;
+	} else if (parent.getChild(word.charAt(0)) != null) {
+	    return removeWordHelper(word.substring(1), parent.getChild(word.charAt(0)));
+	} else {
+	    return false;
+	}
+	
+	public int numWords() {
+	return numWords;
     }
     
-    public int numWords() {
-        return numWords;
-    }
-    
-    public boolean containsWord(String word) {
-        return containsWordHelper(word, root);
+    public boolean containsWord(String word){
+	return containsWordHelper(word, root);
     }
 
-    private boolean containsHelper(String word, LexiconNode parent) {
-        if (word.length() == 0) {
-            return true;
-        } else if (parent.getChild(word.charAt(0) != null)) {
-            return containsWordHelper(word.substring(1), parent.getChild(word.charAt(0)));
-        } else {
-            return false;
-        }
+    private boolean containsWordHelper(String word, LexiconNode parent) {
+	if (word.length() == 0 && parent.isWord()) {
+	    return true;
+	} else if (parent.getChild(word.charAt(0)) != null) {
+	    return containsWordHelper(word.substring(1), parent.getChild(word.charAt(0)));
+	} else {
+	    return false;
+	}
     }
     
-    public boolean containsPrefix(String prefix) {
-        return containsHelper(prefix, root);
-    }    
+    public boolean containsPrefix(String prefix){
+	return containsPrefixHelper(prefix, root);
+    }
+
+    private booolean containsPrefixHelper(String prefix, LexiconNode parent) {
+	if (prefix.length() == 0) {
+	    return true;
+	} else if (parent.getChild(word.charAt(0)) != null) {
+	    return containsPrefixHelper(prefix.substring(1), parent.getChild(word.charAt(0)));
+	} else {
+	    return false;
+	}
+    }
     
     public Iterator<String> iterator() {
-        return null;
+	return null;
     }
-    
     public Set<String> suggestCorrections(String target, int maxDistance) {
-        return null;
+	return null;
     }
     
-    public Set<String> matchRegex(String pattern) {
-        return null;
+    public Set<String> matchRegex(String pattern){
+	return null;
     }
 }
